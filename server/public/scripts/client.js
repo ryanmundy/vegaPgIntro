481057fb043eddfe51419ade67d6682c8c903ba9
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-19 16:57:11 -0600

    day one complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,48 @@ $( document ).ready( readyNow );
 
 function readyNow(){
     console.log( 'JQ' );
+    $('#addSongButton').on('click', addSong);
+    getSongs();
 } // end readynow
+
+function addSong() {
+    console.log('in addSong');
+    //get user input
+    //package in an object
+    
+    const objectToSend = {
+        artist: $('#artistIn').val(),
+        published: $('#publishedIn').val(),
+        rank: $('#rankIn').val(),
+        track: $('#trackIn').val()
+    }//end objectToSend
+    console.log('sending:', objectToSend );
+    //send to server via AJAX
+    $.ajax({
+        method: 'POST',
+        url: '/songs',
+        data: objectToSend
+    }).then( function(response){
+        console.log('back from POST with:', response);
+        
+    }).catch(function(err){
+        console.log('error with POST:', err);
+        
+    })//end ajax
+}//end addSong
+
+function getSongs(){
+    $.ajax({
+        method:'GET',
+        url:'/songs'
+    }).then(function(response){
+        console.log('back from GET with:', response);
+        for(song of response){
+            $('#songs').append(`<li>${song.artist} ${song.published} ${song.rank} ${song.track}</li>`);
+        }
+        
+    }).catch(function(err){
+        console.log('error getting data', err);
+        
+    })
+}
--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.listen( port, ( req, res )=>{
 });
 
 //test route
-app.get('/test', (req, res)=>{
+app.get('/songs', (req, res)=>{
     console.log('/test GET hit');
     //create a query
     const queryString = 'SELECT * FROM songs;';
@@ -48,3 +48,18 @@ app.get('/test', (req, res)=>{
     })//end query
     //res.send('growl');
 });//end test route
+
+app.post('/songs', (req, res)=>{
+    console.log('in /songs POST:', req.body);
+    //res.send('chirp');
+    //create query string
+    const queryString = 'INSERT INTO songs (artist, track, rank, published) VALUES ($1, $2, $3, $4);';
+    pool.query(queryString, [ req.body.artist, req.body.track, req.body.rank, req.body.published ]).then(()=>{
+        res.sendStatus(201);
+    }).catch((err)=>{
+        console.log('error writing song:', err);
+        res.sendStatus(500);
+        
+    });//end query
+    
+})//end /songs POST
